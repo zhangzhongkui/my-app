e67e1c3cfd08961834c7e8972c5ec60599c469ef
ThomasZhang <zhangzk@amazon.com>
2020-12-04 11:24:48 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { API, graphqlOperation } from "@aws-amplify/api";
+
+import awsconfig from "./aws-exports";
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo } from "./graphql/subscriptions";
+import {useState} from "react";
+
+Amplify.configure(awsconfig);
+
 function App() {
+  const [des, setDes] = useState("Learn React");
+  async function createNewTodo() {
+    const todo = {
+      name: "Test",
+      description: `Realtime (${new Date().toLocaleString()})`,
+    };
+    return await API.graphql(graphqlOperation(createTodo, { input: todo }));
+  }
+
+  const onClickHandler = () => {
+    createNewTodo().then((evt) => {
+      setDes(evt.data.createTodo.description)
+      console.log(evt.data.createTodo.name, evt.data.createTodo.description);
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -15,8 +40,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {des}
         </a>
+        <button onClick={onClickHandler} name={"create"}>Change Text</button>
       </header>
     </div>
   );
